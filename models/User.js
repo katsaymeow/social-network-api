f707de04b631d097666337b2cba8d61ace65e49d
katsaymeow <107221758+katsaymeow@users.noreply.github.com>
2022-11-20 20:39:11 -0500

    added user model

--- models/User.js
+++ models/User.js
@@ -1,11 +1,45 @@
 const { Schema, model } = require('mongoose');
-
+const validatorPackage = require('validator');
+const Thought = require('./Thought');
+// User Model
 const userSchema = new Schema(
- 
+    {
+        userName: {
+            type: String,
+            uniqueItems: true,
+            required: [true, "Please enter a valid user name."],
+            trimmed: true,
+        },
+        email: {
+            type: String,
+            required: [true, "Please enter a valid email"],
+            uniqueItems: true,
+            validate: {
+                validator: validatorPackage.isEmail,
+                message: "Please use a valid email",
+            },
+        },
+        thoughts: {
+            thought_id: [Thought],
+        },
+        friends: {
+            friends_id: [User],
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        }
+    }
 );
 
-// Add virtual
-
-// Make model of User
+// virtual function for getting friend count
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length;
+    });
+// The model of User
+const User = model('user', userSchema);
 
 module.exports = User;
